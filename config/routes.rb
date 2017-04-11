a6c8476d697cb319cfc9b9dc4b917df69f2b1b9b
Michelle Golle <michelleg54@gmail.com>
2017-04-11 12:48:18 -0600

    user login route corrected

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root to: 'site#index'
+
+  devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
 end
